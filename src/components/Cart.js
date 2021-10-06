5f0f1265ff257a658d85b4b4015a79a46ae44f0a
Edin Sikira <edinsikira5@gmail.com>
2021-10-06 15:55:24 +0200

    Cart styles added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,10 @@
 import { BsCart3 } from "react-icons/bs";
 import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
-
+import { Link } from "react-router-dom";
+import tempImage from "../images/xx99-mark-II.png";
 function Cart() {
   return (
-    <>
+    <div className="cart-wrapper">
       <button type="button" className="cart-btn">
         <BsCart3 />
       </button>
@@ -14,28 +15,39 @@ function Cart() {
             Remove all
           </button>
         </h4>
-        <div className="cart-items">
-          <article className="cart-item">
+        <div className="items">
+          <article className="item">
             <div className="img-wrapper">
-              <img src="" alt="" />
+              <img src={tempImage} alt="" />
             </div>
             <div className="details">
-              <h5 className="name">zx99</h5>
-              <span className="price"></span>
+              <h5 className="name">
+                zx99 Lorem ipsum dolor sit amet, consectetur
+              </h5>
+              <span className="price">$399</span>
+              <button type="button" className="remove-item-btn">
+                remove
+              </button>
             </div>
             <div className="quantity-btns">
-              <button type="button" className="change-quantity">
+              <button type="button">
                 <IoIosArrowUp />
               </button>
               <span className="quantity">0</span>
-              <button type="button" className="change-quantity">
+              <button type="button">
                 <IoIosArrowDown />
               </button>
             </div>
           </article>
         </div>
+        <div className="total">
+          TOTAL <span>$399</span>
+        </div>
+        <button type="button" className="checkout-btn">
+          <Link to="/checkout">checkout</Link>
+        </button>
       </section>
-    </>
+    </div>
   );
 }
 
