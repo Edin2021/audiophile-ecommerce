9dd9d2cc37b975c6475f30b1819adb320c791008
Edin Sikira <edinsikira5@gmail.com>
2021-10-09 17:24:37 +0200

    Test

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,18 +1,16 @@
+import { useParams } from "react-router";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Product from "../components/Product";
 
 function Products() {
-  console.log(window.location.pathname);
+  const currCategory = useParams("category").category;
+  console.log(currCategory);
   return (
     <>
       <Header flag={"category"} />
       {/* dinamically add products */}
       <main className="products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
         <Product />
       </main>
       <Footer />
