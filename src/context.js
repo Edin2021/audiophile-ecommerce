352776288603533451040541e4803c7261a9f72c
Edin Sikira <edinsikira5@gmail.com>
2021-10-10 14:32:31 +0200

    Test

--- src/context.js
+++ src/context.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const AppContext = React.createContext();
 
 export default function AppProvider({ children }) {
-  const temp = "temp";
-  return <AppContext.Provider value={temp}>{children}</AppContext.Provider>;
+  const history = useHistory();
+  const prevPage = () => {
+    history.goBack();
+  };
+  return (
+    <AppContext.Provider value={{ prevPage }}>{children}</AppContext.Provider>
+  );
 }
 
 export const useGlobalContext = () => React.useContext(AppContext);
